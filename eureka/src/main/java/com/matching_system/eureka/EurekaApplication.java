5f729dec3af59e4f3fb080f1204492ede720707b
GodtoL <eliasvera793@gmail.com>
2024-09-19 19:53:46 -0400

    Configuración del server de eureka

---
+++ eureka/src/main/java/com/matching_system/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.matching_system.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
---
+++ eureka/src/test/java/com/matching_system/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matching_system.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
